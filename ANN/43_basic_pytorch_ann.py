ccb1e880bc5d3a8a2bba479f3a492b2e364e61a2
Akila Mendis <akila@oneit.com.au>
2021-09-15 13:19:16 +0530

    45 Basic pytorch part three

--- ANN/43_basic_pytorch_ann.py
+++ ANN/43_basic_pytorch_ann.py
@@ -92,3 +92,52 @@ plt.plot(range(epochs), losses)
 plt.ylabel('LOSS')
 plt.xlabel('Epoch')
 plt.show()
+
+with torch.no_grad():
+    y_eval = model.forward(X_test)
+    loss = criterion(y_eval, y_test)
+
+print(loss)
+correct = 0
+
+with torch.no_grad():
+    for i, data in enumerate(X_test):
+        y_val = model.forward(data)
+        # print(f'{i+1}.) {str(y_val)}    {y_test[i]}')
+        print(f'{i + 1}.) {str(y_val.argmax().item())}    {y_test[i]}')
+        if y_val.argmax().item() == y_test[i]:
+            correct += 1
+
+print(f'We got {correct} correct!')
+
+torch.save(model.state_dict(), '../Data/my_iris_model.pt')
+
+new_model = Model()
+new_model.load_state_dict(torch.load('../Data/my_iris_model.pt'))
+print(new_model.eval())
+
+mystery_iris = torch.tensor([5.6, 3.7, 2.2, 0.5])
+
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10,7))
+fig.tight_layout()
+
+plots = [(0, 1), (2, 3), (0, 2), (1, 3)]
+colors = ['b', 'r', 'g']
+labels = ['Iris setosa', 'Iris virginica', 'Iris versicolor']
+
+for i, ax in enumerate(axes.flat):
+    for j in range(3):
+        x = df.columns[plots[i][0]]
+        y = df.columns[plots[i][1]]
+        ax.scatter(df[df['target']==j][x], df[df['target']==j][y], color=colors[j])
+        ax.set(xlabel=x, ylabel=y)
+
+    # Add a plot for our mystery iris
+    ax.scatter(mystery_iris[plots[i][0]], mystery_iris[plots[i][1]], color='y')
+
+fig.legend(labels=labels, loc=3, bbox_to_anchor=(1.0, 0.85))
+plt.show()
+
+with torch.no_grad():
+    print(new_model(mystery_iris))
+    print(new_model(mystery_iris).argmax().item())
